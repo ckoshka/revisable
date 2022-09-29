fd5065b2cb72bb2cf161aea1b3099fca17c9c668
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-09-29 20:14:50 +1300

    sorted out weird behaviour with type unions

--- revisable.ts
+++ revisable.ts
@@ -1,28 +1,40 @@
+
 export type Revisable<T> = {
 	revise: (settings: Partial<T>) => Revisable<T>;
-	map: <K extends keyof T>(
+	map: <K extends keyof T, Output>(
 		key: K,
-	) => (fn: (a0: T[K]) => T[K]) => Revisable<T>;
+		fn: (a0: T[K]) => Output,
+	) => Revisable<Omit<T, K> & Record<K, Output>>;
+	mapR: <K extends keyof T, Output>(
+		key: K,
+		fn: (a0: Revisable<T[K]>) => Revisable<Output>,
+	) => Revisable<Omit<T, K> & Record<K, Output>>;
 	extend: <Rec extends Record<string, unknown>>(
 		mapfn: (a0: T) => Rec,
-	) => Revisable<T & Rec>;
+	) => Revisable<Omit<T, keyof Rec> & Rec>;
 	contents: T;
 };
 
 export const revisable = <T>(t: T): Revisable<T> => ({
-	contents: t,
-	map: <K extends keyof T>(key: K) =>
-		(fn: (a0: T[K]) => T[K]) =>
-			revisable({
-				...t,
-				...{ [key]: fn(t[key] as T[K]) } as Record<K, T[K]>,
-			}),
+	contents: { ...t },
+	map: <K extends keyof T, Output>(key: K, fn: (a0: T[K]) => Output) =>
+		revisable({
+			...t,
+			...{ [key]: fn(t[key]) } as Record<K, Output>,
+		}) as Revisable<Omit<T, K> & Record<K, Output>>,
+	mapR: <K extends keyof T, Output>(
+		key: K,
+		fn: (a0: Revisable<T[K]>) => Revisable<Output>,
+	) => revisable({
+		...t,
+		...{ [key]: fn(revisable(t[key])).contents } as Record<K, Output>,
+	}) as Revisable<Omit<T, K> & Record<K, Output>>,
 	extend: <Rec extends Record<string, unknown>>(mapfn: (a0: T) => Rec) =>
 		revisable(
 			{
 				...t,
 				...mapfn(t),
-			} as T & Rec,
+			} as Omit<T, keyof Rec> & Rec,
 		),
 	revise: (settings: Partial<T>) =>
 		revisable({
--- tests/1.ts
+++ tests/1.ts
@@ -1,3 +1,25 @@
 import { revisable } from "../revisable.ts";
 
-revisable(null).map(``)
+`
+revisable(null).map("key")
+`;
+
+const x = revisable({
+	domainName: "https://watch-hamsters-cagefight.com",
+	videoName: `Fluffy annihilated in 1:1 with Chestnut`,
+	checkCache: true,
+	storageSettings: {
+		directory:
+			"/Users/ckoshka/videos/rodents/hamsters/fluffy_1v1_chestnut.mp4",
+	},
+})
+	.map("checkCache", (b) => !b)
+	.mapR("storageSettings", (settings) =>
+		settings.map("directory", (dir) => dir.replace(".mp4", ".mp4.gz"))
+	)
+	.extend(() => ({ checkCache: 4 }))
+	.extend(({ domainName }) =>
+		domainName.startsWith("https") ? { encrypt: true } : { encrypt: false }
+	)
+    .map("encrypt", bool => bool ? "SHA-1" : "none")
+type B = typeof x.contents.checkCache;
