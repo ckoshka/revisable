7f73566bdd700472eabed1d74f583a5c8df03e38
fastelectronicvegetable <48640397+ckoshka@users.noreply.github.com>
2022-10-01 10:57:06 +1300

    allows for safely changing the type of a nested obj

--- revisable.ts
+++ revisable.ts
@@ -1,14 +1,16 @@
-
 export type Revisable<T> = {
 	revise: (settings: Partial<T>) => Revisable<T>;
+
 	map: <K extends keyof T, Output>(
 		key: K,
 		fn: (a0: T[K]) => Output,
 	) => Revisable<Omit<T, K> & Record<K, Output>>;
+
 	mapR: <K extends keyof T, Output>(
 		key: K,
 		fn: (a0: Revisable<T[K]>) => Revisable<Output>,
 	) => Revisable<Omit<T, K> & Record<K, Output>>;
+
 	extend: <Rec extends Record<string, unknown>>(
 		mapfn: (a0: T) => Rec,
 	) => Revisable<Omit<T, keyof Rec> & Rec>;
@@ -17,11 +19,13 @@ export type Revisable<T> = {
 
 export const revisable = <T>(t: T): Revisable<T> => ({
 	contents: { ...t },
+
 	map: <K extends keyof T, Output>(key: K, fn: (a0: T[K]) => Output) =>
 		revisable({
 			...t,
 			...{ [key]: fn(t[key]) } as Record<K, Output>,
 		}) as Revisable<Omit<T, K> & Record<K, Output>>,
+
 	mapR: <K extends keyof T, Output>(
 		key: K,
 		fn: (a0: Revisable<T[K]>) => Revisable<Output>,
@@ -29,6 +33,7 @@ export const revisable = <T>(t: T): Revisable<T> => ({
 		...t,
 		...{ [key]: fn(revisable(t[key])).contents } as Record<K, Output>,
 	}) as Revisable<Omit<T, K> & Record<K, Output>>,
+
 	extend: <Rec extends Record<string, unknown>>(mapfn: (a0: T) => Rec) =>
 		revisable(
 			{
@@ -36,6 +41,7 @@ export const revisable = <T>(t: T): Revisable<T> => ({
 				...mapfn(t),
 			} as Omit<T, keyof Rec> & Rec,
 		),
+
 	revise: (settings: Partial<T>) =>
 		revisable({
 			...t,
